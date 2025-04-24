06a930b0c0a376c9a87a8a6e0ffaf273d63adc88
RGB39 <79316659+reallyGoodBaker@users.noreply.github.com>
2025-04-24 10:32:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Superego
+
+Developed with Unreal Engine 5
---
+++ Source/Superego/Superego.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Superego.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Superego, "Superego" );
